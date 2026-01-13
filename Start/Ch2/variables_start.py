34775059f373961219aec3b078cbf1d8d2a2935b
ToriGit12 <bostic57@students.rowan.edu>
2026-01-13 13:50:48 -0500

    Completed variables lesson for Learning Python

--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,37 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+#print(myint)
+#print(mystr)
 
 # Operators are used to perform operations on variables
+# print(myint + myfloat)
+# print(myint * myfloat)
+# print(myint / myfloat)
+# print(myint % 3)  remainder should be 1
 
+# another_str = "This is another string"
+# print(mystr + another_str)
+# print("nom " * 3)
+# print(mystr + 1)  you cannot add two fundamentally different data types
 
 # Logical and comparison operators 
+# print(myint == 10)  '==' is for comparing, '=' is for assigning; answer True
+# print(myfloat != 20)
+# print(myint > 20)
+# print(myfloat < 10)
 
+# AND True: Both statements are true
+# AND False: One statement true, the other false, or both are false
+# print(myint > 5 and myfloat < 25.0)
+
+# True because in the case of OR operator,
+# only one of the results has to be true for the result to be "True"
+# print(myint < 5 or myfloat < 25.0)
+
+# NOT changes the result to its opposite
+# print(not (myint < 5 or myfloat < 25.0))
 
 # re-declaring a variable works
+myint = "abc"
+print(myint)
