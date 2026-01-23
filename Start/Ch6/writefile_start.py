d2b7b140b8714828fdf7efcfe9fc65f7ae773a98
ToriGit12 <bostic57@students.rowan.edu>
2026-01-23 17:48:10 -0500

    Finished writefile lesson for Learning Python with file example

--- Start/Ch6/writefile_start.py
+++ Start/Ch6/writefile_start.py
@@ -3,13 +3,20 @@
 #
 
 
-# Open a file for writing and create it if it doesn't exist
+# Open a file for writing (open()) and create it if it doesn't exist ("w+")
+# sample_file = open("textfile.txt", "w+")
+# # Write some data to the file
+# sample_file.write("This is some sample text in our sample file.")
+# sample_file.write("Some more sample text.")
+# # Now close it
+# sample_file.close()
 
-
-# Open the file for appending text to the end
-
+# Open the file for appending text to the end ("a+")
+sample_file = open("textfile.txt", "a+")
 
 # write some lines of data to the file
-
+sample_file.write("Some more sample text.")
+sample_file.write("Even more sample text.")
 
 # close the file when done
+sample_file.close()
