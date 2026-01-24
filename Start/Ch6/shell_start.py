b2fadfd8cfd87b259ba87e5a9800a4b5c2f12d7f
ToriGit12 <bostic57@students.rowan.edu>
2026-01-24 07:33:47 -0500

    Finished shell lesson for Learning Python with new files

--- Start/Ch6/shell_start.py
+++ Start/Ch6/shell_start.py
@@ -2,23 +2,37 @@
 # Example file for working with filesystem shell methods
 # LinkedIn Learning Python course by Joe Marini
 #
-
+import os
+from os import path
+import shutil
+from zipfile import ZipFile
 
 # make a duplicate of an existing file
-
+if path.exists("newfile.txt"):
     # get the path to the file in the current directory
-
+    src = path.realpath("newfile.txt")
         
     # # let's make a backup copy by appending "bak" to the name
+    # dst = src + ".bak"
 
     # # now use the shell to make a copy of the file
-
+    # shutil.copy() copies over the file's content,
+    # but does not copy over the original file's metadata,
+    # such as the creation and modification times and other properties
+    # shutil.copy(src, dst)
 
     # # rename the original file
-
+    # os.rename("textfile.txt", "newfile.txt")
     
     # now put things into a ZIP archive
-
+    # root_dir, tail = path.split(src)
+    # shutil.make_archive("archive", "zip", root_dir)
 
     # more fine-grained control over ZIP files
-      
+    # with keyword lets you create a local scope
+    # that simplifies working with objects
+    with ZipFile("testzip.zip", "w") as newzip: # creates a context manager
+        newzip.write("newfile.txt")
+        newzip.write("textfile.txt.bak")
+        # when the two lines above finish executing, 
+        # the with statement will automatically close the zip file
