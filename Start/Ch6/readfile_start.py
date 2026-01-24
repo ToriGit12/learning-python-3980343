a389e71f2e541e269eed3a6b665fb9ad62d9e0ec
ToriGit12 <bostic57@students.rowan.edu>
2026-01-24 06:48:03 -0500

    Finished readfile lesson for Learning Python

--- Start/Ch6/readfile_start.py
+++ Start/Ch6/readfile_start.py
@@ -5,5 +5,13 @@
 
     
 # Open the file and read the contents
+sample_file = open("textfile.txt", "r")
 
+# If sample_file's mode is "read", file was opened correctly
+if sample_file.mode == 'r':
     # use the read() function to read the entire file
+    # contents = sample_file.read()
+    # print(contents)
+    file_lines = sample_file.readlines()
+    for line in file_lines:
+        print(line)
