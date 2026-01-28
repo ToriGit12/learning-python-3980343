e8b8c0f2be3b3d21899d82f80b24ee67b51c3087
ToriGit12 <bostic57@students.rowan.edu>
2026-01-28 10:49:25 -0500

    Finished jsondata lesson for Learning Python

--- Start/Ch7/jsondata_start.py
+++ Start/Ch7/jsondata_start.py
@@ -3,12 +3,21 @@
 #
 
 import urllib.request 
-
+import json
 
 # Open the URL and read the data
-
+web_url = urllib.request.urlopen("https://uselessfacts.jsph.pl/api/v2/facts/random")
+print("Result code:", web_url.getcode())
 
 # Read the JSON data from the source
-
+data = web_url.read()
+# print(data)
 
 # Print the content of the 'text' field
+
+# This line takes the string of JSON code
+# and parses it into a native Python dictionary object
+theJSON = json.loads(data) # "load s" means "load string"
+
+# Now access the data within the object
+print(theJSON["text"]) # access dictionary value with key
