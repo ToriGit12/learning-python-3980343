05f393898e8b78b07c7f9b873c7c9241fdbd0f77
ToriGit12 <bostic57@students.rowan.edu>
2026-01-28 10:31:55 -0500

    Completed inetdata lesson for Learning Python

--- Start/Ch7/inetdata_start.py
+++ Start/Ch7/inetdata_start.py
@@ -1,3 +1,15 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for retrieving data from the internet
 #
+
+import urllib.request
+
+# Open a URL and get the HTTP result code
+# 200 if working, 404 if there is an error
+web_url = urllib.request.urlopen("http://www.example.com")
+print("Result code:", web_url.getcode())
+
+# Read the entire contents of the provided URL
+# Comes out in HTML
+data = web_url.read()
+print(data)
